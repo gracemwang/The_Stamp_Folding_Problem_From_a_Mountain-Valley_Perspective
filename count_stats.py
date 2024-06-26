0ed166a3824b0d5619ce71668627cfc36dc5674d
Adham Ibrahim <adham.ibrahim@princeton.edu>
2024-06-26 19:53:06 -0400

    more figures!

--- count_stats.py
+++ count_stats.py
@@ -3,39 +3,42 @@ Analyze counts.txt and make a histogram of the counts.
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-N = 10
+N = 22
 
 strings = {}
 numbers = {}
 maxes = {}
 
-for n in range(4, 21):
+for n in range(4, N+1):
     file_path = '1xn_data/1x{}counts.txt'.format(n)
 
     with open(file_path, 'r') as file:
         numbers[n] = []
         strings[n] = []
         for line in file:
-            numbers[n].append(int(line.strip().split(' ')[1]))
-            strings[n].append(line.strip().split(' ')[0])
+            if ' ' in line:
+                numbers[n].append(int(line.strip().split(' ')[1]))
+                strings[n].append(line.strip().split(' ')[0])
+            else:
+                numbers[n].append(int(line.strip()))
         maxes[n] = max(numbers[n])
 
     # print(strings[n])
     # print(numbers[n])
     # print(maxes[n])
 
-    # print(numbers[6][3] == maxes[6])
-    # print(strings[6][3])
+    # for i in range(len(numbers[n])):
+    #     if numbers[n][i] == maxes[n]:
+    #         print(strings[n][i], end=' ')
+    # print()
 
-    for i in range(len(numbers[n])):
-        if numbers[n][i] == maxes[n]:
-            print(strings[n][i], end=' ')
-    print()
-
-# plt.figure(figsize=(10, 6))
-# plt.hist(numbers[N], bins=maxes[N], edgecolor='black')
-# plt.xlabel('# of ways to fold')
-# plt.ylabel("# of MV assignments that achieve X ways to fold")
-# plt.title("The 1x21 case")
-# plt.show()
+for n in range(10, 23):
+    plt.figure(figsize=(10, 6))
+    plt.hist(numbers[n], bins= maxes[n], edgecolor='black')
+    plt.xlabel('# of ways to fold')
+    plt.ylabel("# of MV assignments that achieve X ways to fold")
+    plt.title("The 1x{} case".format(n))
+    plt.savefig('1xn_figures/1x{}_distribution.jpg'.format(n))
+    # plt.show()
